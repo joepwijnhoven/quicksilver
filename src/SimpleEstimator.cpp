64fa1ede6d81727ea8e8ad78763abd3cfc1930c0
Mikedebrouwer <Mikedebrouwer@gmail.com>
2018-03-10 21:15:14 +0100

    commented out old code

--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -12,8 +12,8 @@ SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g){
 
     // works only with SimpleGraph
     graph = g;
-    nr_edges_in.resize(graph.get()->getNoVertices());
-    nr_edges_out.resize(graph.get()->getNoVertices());
+//    nr_edges_in.resize(graph.get()->getNoVertices());
+//    nr_edges_out.resize(graph.get()->getNoVertices());
     nr_label_occurences.resize(graph.get()->getNoLabels());
     array.resize(graph.get()->getNoLabels());
     tabels.resize(graph.get()->getNoLabels());
@@ -42,23 +42,23 @@ void SimpleEstimator::prepare() {
         }
     }
 
-    std::cout << array[0] << " " << array[1] << " " << array[2] << " " << array[3] << "\n";
+    std::cout << array[0] << " " << array[1] << " " << array[8] << " " << array[9] << "\n";
     std::cout << "HERE: " << uniqueIN[0].size() << " " << uniqueOUT[0].size();
     std::cout << "HERE: " << uniqueIN[1].size() << " " << uniqueOUT[1].size();
-    std::cout << "HERE: " << uniqueIN[2].size() << " " << uniqueOUT[2].size();
-    std::cout << "HERE: " << uniqueIN[3].size() << " " << uniqueOUT[3].size();
+    std::cout << "HERE: " << uniqueIN[8].size() << " " << uniqueOUT[8].size();
+    std::cout << "HERE: " << uniqueIN[9].size() << " " << uniqueOUT[9].size();
 
 
 
     // For each node count the number of outgoing and incoming edges
-    for(int source = 0; source < graph->getNoVertices(); source++) {
-        nr_edges_out[source] = graph->adj[source].size();
-        std::cout << "size of: " << graph->adj[source].data() << " number edges: " << nr_edges_out[source] << "\n";
-    }
-
-    for(int target = 0; target < graph->getNoVertices(); target++) {
-        nr_edges_in[target] = graph->reverse_adj[target].size();
-    }
+//    for(int source = 0; source < graph->getNoVertices(); source++) {
+//        nr_edges_out[source] = graph->adj[source].size();
+//        std::cout << "size of: " << graph->adj[source].data() << " number edges: " << nr_edges_out[source] << "\n";
+//    }
+//
+//    for(int target = 0; target < graph->getNoVertices(); target++) {
+//        nr_edges_in[target] = graph->reverse_adj[target].size();
+//    }
 
 //    //for each unique label count the number of occurences
 //    for(int label = 0; label < graph->getNoLabels(); label++) {
@@ -91,6 +91,7 @@ std::vector<int> SimpleEstimator::estimatePath(RPQTree *q) {
         auto left = estimatePath(q->left);
         auto right = estimatePath(q->right);
         std::vector<int> test {left[0], right[1], (int) min((left[2] * (((double)right[2])/((double)right[0]))), right[2] * ((double)left[2]/(double)left[1]))};
+        std::cout << "HERE test 2: " << test[2];
         return test;
     }
     return {};
@@ -99,30 +100,30 @@ std::vector<int> SimpleEstimator::estimatePath(RPQTree *q) {
 cardStat SimpleEstimator::estimate(RPQTree *q) {
     // perform your estimation here
     // variables to be returned later
-    uint32_t noIn;
-    uint32_t noOut;
-    uint32_t noPaths;
+//    uint32_t noIn;
+//    uint32_t noOut;
+//    uint32_t noPaths;
 
     /* check what the first and last node are in query, because we can assume s and t can be anything for
      * noOut we look at all the edges corresponding to the first label in the query. Similar thing is done for noIn */
 
-    RPQTree *q_in = q;
-    RPQTree *q_out = q;
-    // retrieve the begin of the query
-    while (q_in->isConcat()) {
-         q_in = q_in->left;
-    }
-    // get the int value of the node and the direction, direction not needed yet.
-    std::string query_begin = q_in->data;
-    int query_begin_edge = atoi(query_begin.substr(0, query_begin.length() - 1).c_str());
-    char query_begin_direction = query_begin.at(query_begin.length() - 1);
-
-    while (q_out->isConcat() == 1) {
-        q_out = q_out->right;
-    }
-    std::string query_end = q_out->data;
-    int query_end_edge = atoi(query_end.substr(0, query_end.length() - 1).c_str());
-    char query_end_direction = query_end.at(query_end.length() - 1);
+//    RPQTree *q_in = q;
+//    RPQTree *q_out = q;
+//    // retrieve the begin of the query
+//    while (q_in->isConcat()) {
+//         q_in = q_in->left;
+//    }
+//    // get the int value of the node and the direction, direction not needed yet.
+//    std::string query_begin = q_in->data;
+//    int query_begin_edge = atoi(query_begin.substr(0, query_begin.length() - 1).c_str());
+//    char query_begin_direction = query_begin.at(query_begin.length() - 1);
+//
+//    while (q_out->isConcat() == 1) {
+//        q_out = q_out->right;
+//    }
+//    std::string query_end = q_out->data;
+//    int query_end_edge = atoi(query_end.substr(0, query_end.length() - 1).c_str());
+//    char query_end_direction = query_end.at(query_end.length() - 1);
 
 //    noOut = nr_edges_in[query_begin_edge];
 //
@@ -133,8 +134,8 @@ cardStat SimpleEstimator::estimate(RPQTree *q) {
 //        noIn = nr_edges_in[query_end_edge];
 //    }
 
-    noOut = graph.get()->getNoEdges()/graph.get()->getNoLabels();
-    noIn = graph.get()->getNoEdges()/graph.get()->getNoLabels();
+//    noOut = graph.get()->getNoEdges()/graph.get()->getNoLabels();
+//    noIn = graph.get()->getNoEdges()/graph.get()->getNoLabels();
     std::vector<int> testvar = estimatePath(q);
     //noPaths = uint32_t(estimatePath(q)[2]);
     return cardStat {testvar[0],testvar[2],testvar[1]};
